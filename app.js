8eec9fc0bdaeb5a1b508af9e141395b17cdf3c86
sourob2356 <sourob2356@gmail.com>
2025-07-12 16:42:35 +0600

    feat: add note router to API for note management

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser')
 const sessionRouter = require('./routes/session.router');
 const userRouter = require('./routes/user.router');
 const materialRouter = require('./routes/material.router');
+const noteRouter = require('./routes/note.router');
 
 const app = express();
 app.use(express.json());
@@ -19,6 +20,7 @@ app.use(cors({
 app.use('/api/v1/user',  userRouter);
 app.use('/api/v1/sessions',  sessionRouter); 
 app.use('/api/v1/materials',  materialRouter); 
+app.use('/api/v1/notes',  noteRouter); 
 
 
 
